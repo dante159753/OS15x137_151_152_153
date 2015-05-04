38e3f17eafa7e12360279542c5b217ba60f10c00
Wangyan Zhao <wyz159753@gmail.com>
2015-05-04 09:54:18 +0800

    complete Alarm Clock and Priority Scheduling

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -20,6 +20,9 @@
 /* Number of timer ticks since OS booted. */
 static int64_t ticks;
 
+/* 定义一个sleep_list链表，用于存储休眠的进程 */
+static struct list sleep_list;
+
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
@@ -45,6 +48,8 @@ timer_init (void)
   outb (0x40, count >> 8);
 
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+  /* 初始化sleep_list链表 */
+  list_init(&sleep_list);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
@@ -91,18 +96,6 @@ int64_t
 timer_elapsed (int64_t then) 
 {
   return timer_ticks () - then;
-}
-
-/* Sleeps for approximately TICKS timer ticks.  Interrupts must
-   be turned on. */
-void
-timer_sleep (int64_t ticks) 
-{
-  int64_t start = timer_ticks ();
-
-  ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -174,14 +167,7 @@ timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
-/* Timer interrupt handler. */
-static void
-timer_interrupt (struct intr_frame *args UNUSED)
-{
-  ticks++;
-  thread_tick ();
-}
+
 
 /* Returns true if LOOPS iterations waits for more than one timer
    tick, otherwise false. */
@@ -252,4 +238,94 @@ real_time_delay (int64_t num, int32_t denom)
      the possibility of overflow. */
   ASSERT (denom % 1000 == 0);
   busy_wait (loops_per_tick * num / 1000 * TIMER_FREQ / (denom / 1000)); 
+}
+
+/* 自定义一个list_less_func *函数，用于插入排序 */
+bool ticks_cmp(
+    const struct list_elem *a,
+    const struct list_elem *b,
+    void *aux UNUSED
+) {
+    struct thread *ta = list_entry(a, struct thread, elem);
+    struct thread *tb = list_entry(b, struct thread, elem);
+
+    /* 在这里使用升序 */
+    if ((ta -> ticks) > (tb -> ticks)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/* Sleeps for approximately TICKS timer ticks.  Interrupts must
+   be turned on. */
+void timer_sleep(int64_t ticks) {
+    /* 定义一个thread变量，用于捕获当前线程 */
+    struct thread *get_current_thread;
+    enum intr_level old_level;
+
+    ASSERT (intr_get_level () == INTR_ON);
+
+    /*
+     * 注释掉原有的代码：
+     * while (timer_elapsed (start) < ticks)
+     *   thread_yield ();
+     */
+
+    if (ticks > 0) {
+        old_level = intr_disable();
+        get_current_thread = thread_current();
+        /* 设置当前线程真正需要被唤醒的时钟点 */
+        get_current_thread -> ticks = ticks + timer_ticks();
+
+        /* 排序插入 */
+        list_insert_ordered(
+            &sleep_list,
+            &get_current_thread -> elem,
+            (list_less_func *)&ticks_cmp,
+            NULL
+        );
+        /* 阻塞当前线程 */
+        thread_block();
+        intr_set_level(old_level);
+    }
+}
+
+/* Timer interrupt handler. */
+static void timer_interrupt(struct intr_frame *args UNUSED) {
+    struct list_elem *temp_elem;
+    /* ticks是从OS启动开始就一直存在的，在这里时其自加 */
+    ticks++;
+    /* Called by the timer interrupt handler at each timer tick.
+    Thus, this function runs in an external interrupt context. */
+    thread_tick ();
+
+    /*
+    * 接下来我们可以开始自定义了；
+    * 在这里我们理解为，用于计算ticks的进程（或线程）是一直都存在的，
+    * 所以不需要我们重新开启一个线程去统计时钟，直接在这里计算就可以了
+    *
+    * 那么好，我们之前定义了一个sleep_list，
+    * 这个sleep_list是用来存储当前的睡眠线程的，
+    * 我们在sleep_list中遍历，
+    * 每满足一个线程唤醒的条件（因为可能有多个线程会在同一时间内被唤醒，所以我们使用while()循环），
+    * 我们就将其唤醒
+    */
+
+    /* 也可以调用list_min()函数每次找出最小的那个，这样就不用排序了 */
+    /*
+     * 注意main()为什么要最后一个唤醒呢？
+     * 一个可能的解释是，主线程要等待子线程的运算结果，
+     * 所以在所有的运算结果可达之前，主线程不能先于子线程被唤醒
+     */
+    temp_elem = list_begin(&sleep_list);
+    while (temp_elem != list_end(&sleep_list)) {
+        struct thread *t = list_entry(temp_elem, struct thread, elem);
+        if (ticks < t -> ticks) {
+            break;
+        }
+        list_remove(temp_elem);
+        thread_unblock(t);
+        temp_elem = list_begin(&sleep_list);
+    }
 }
--- src/threads/synch.h
+++ src/threads/synch.h
@@ -41,6 +41,13 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
+/* 用于比较condition variable */
+bool cmp_sem_priority(
+    const struct list_elem *a,
+    const struct list_elem *b,
+    void *aux
+);
+
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
